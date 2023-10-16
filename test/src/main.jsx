e012b767b5cea99af8901f7dce6a0bb6906af729
Akwa <110759413+Akwa-08@user.noreply.github.com>
2023-10-17 00:16:46 +0800

    analyze master detail

--- test/src/main.jsx
+++ test/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BroswerRouter>
-      <App />
+      <App />  
     </BroswerRouter>
   </React.StrictMode>,
 )
